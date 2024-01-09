92cec6b09a07af6673ff1a8c686933d8019e1ac4
femiadefila <liladeoluwa@gmail.com>
2024-01-09 15:08:42 +0100

    your comment

---
+++ script.js
@@ -0,0 +1,328 @@
+const questions = [
+  {
+   question: "Who loves to Bully Prof?",
+   answers: [
+    { text: "Prof", correct: false },
+    { text: "Jumy", correct: false },
+    { text: "Pepepe", correct: false },
+    { text: "Esosa", correct: true },
+   ]
+  },
+ 
+  {
+   question: "Another name for Reign is?",
+   answers: [
+    { text: "Molly", correct: false },
+    { text: "Leo", correct: false },
+    { text: "Yungdaggerdick", correct: true },
+    { text: "Rain", correct: false },
+   ]
+  },
+  {
+   question: "Who made Gloria and Vic to leave?",
+   answers: [
+    { text: "Bola", correct: true },
+    { text: "Mumsi", correct: false },
+    { text: "Popsi", correct: false },
+    { text: "Pepepe", correct: false },
+   ]
+  },
+  {
+    question: "Is Esosa food Sweet?",
+    answers: [
+     { text: "Yes", correct: false },
+     { text: "No", correct: false },
+     { text: "Can do better", correct: false },
+     { text: "Absolute trash", correct: true },
+    ]
+   },
+   {
+    question: "Who is living a double life with itegbojie?",
+    answers: [
+     { text: "Bola", correct: false },
+     { text: "Hope", correct: false },
+     { text: "Prof", correct: true },
+     { text: "Ochez", correct: false },
+    ]
+   },
+   {
+    question: "Who made popsi give us curfew?",
+    answers: [
+     { text: "Mumsi", correct: false },
+     { text: "Hope", correct: false },
+     { text: "Bola", correct: true },
+     { text: "Ochez", correct: false },
+    ]
+   },
+   {
+    question: "The most Hardworking person in the house?",
+    answers: [
+     { text: "Pepepe", correct: false },
+     { text: "Ochez", correct: false },
+     { text: "Prof", correct: true },
+     { text: "Hope", correct: false },
+    ]
+   },
+   {
+    question: "Who blasts every morning non-stop?",
+    answers: [
+     { text: "Bola", correct: false },
+     { text: "Ochez", correct: false },
+     { text: "Pepepe", correct: true },
+     { text: "Popsi", correct: false },
+    ]
+   },
+   {
+    question: "Who cooks better in the house?",
+    answers: [
+     { text: "Bola", correct: false },
+     { text: "Ochez", correct: false },
+     { text: "Hope", correct: false },
+     { text: "Pepepe", correct: true },
+    ]
+   },
+   {
+    question: "Who screams Hallelujah! every morning?",
+    answers: [
+     { text: "Bola", correct: false },
+     { text: "Popsi", correct: true },
+     { text: "Mumsi", correct: false },
+     { text: "Pepepe", correct: false },
+    ]
+   },
+   {
+    question: "Who will still put us in trouble for coming back late at night?",
+    answers: [
+     { text: "Bola", correct: true },
+     { text: "Ochez", correct: false },
+     { text: "Hope", correct: false },
+     { text: "Pepepe", correct: false },
+    ]
+   },
+   {
+    question: "House on the Rock Ambassador?",
+    answers: [
+     { text: "Esosa", correct: false },
+     { text: "Ochez", correct: false },
+     { text: "Bola", correct: false },
+     { text: "Pepepe", correct: true },
+    ]
+   },
+   {
+    question: "Who presses phone the most?",
+    answers: [
+     { text: "Esosa", correct: false },
+     { text: "Jums", correct: true },
+     { text: "Bola", correct: false },
+     { text: "Ochez", correct: false },
+    ]
+   },
+   {
+    question: "Who should stop cooking for us?",
+    answers: [
+     { text: "Mumsi", correct: true },
+     { text: "Hope", correct: false },
+     { text: "Bola", correct: false },
+     { text: "Ochez", correct: false },
+    ]
+   },
+   {
+    question: "Who is the funniest person?",
+    answers: [
+     { text: "Esosa", correct: true },
+     { text: "Pepepe", correct: false },
+     { text: "Prof", correct: false },
+     { text: "Ochez", correct: false },
+    ]
+   },
+   {
+    question: "The person with the biggest head?",
+    answers: [
+     { text: "Ayanfe", correct: false },
+     { text: "Pepepe", correct: false },
+     { text: "Jums", correct: false },
+     { text: "Ochez", correct: true },
+    ]
+   },
+
+   {
+    question: "Who should not have boyfriends?",
+    answers: [
+     { text: "Ochez", correct: false },
+     { text: "Jums and Bola", correct: true },
+     { text: "Hope", correct: false },
+     { text: "Esosa", correct: false },
+    ]
+   },
+
+   {
+    question: "Who can watch game till eternity?",
+    answers: [
+     { text: "Prof", correct: false },
+     { text: "Ayanfe", correct: true },
+     { text: "Jums", correct: false },
+     { text: "Esosa", correct: false },
+    ]
+   },
+   {
+    question: "Who tries to make good jokes sometimes?",
+    answers: [
+     { text: "Prof", correct: false },
+     { text: "Bola", correct: false },
+     { text: "Pepepe", correct: true },
+     { text: "Esosa", correct: false },
+    ]
+   },
+   {
+    question: "Who requires the longest preparation time before heading out?",
+    answers: [
+     { text: "Hope", correct: false },
+     { text: "Bola", correct: false },
+     { text: "Oji", correct: false },
+     { text: "Ochez", correct: true },
+    ]
+   },
+   {
+    question: "If you keep snacks in the fridge who always eat it?",
+    answers: [
+     { text: "Pepepe", correct: false },
+     { text: "Prof", correct: true },
+     { text: "Jums", correct: false },
+     { text: "Ochez", correct: false },
+    ]
+   },
+   {
+    question: "Who needs to come back home ASAP?",
+    answers: [
+     { text: "Hope", correct: false },
+     { text: "Ineose", correct: false },
+     { text: "Ochez", correct: true },
+     { text: "Oji", correct: false },
+    ]
+   },
+   {
+    question: "Who talks the most?",
+    answers: [
+     { text: "Hope", correct: false },
+     { text: "Bola", correct: false },
+     { text: "Ochez", correct: false },
+     { text: "Oji", correct: true },
+    ]
+   },
+   {
+    question: "Married to someone in tech?",
+    answers: [
+     { text: "Jums", correct: false },
+     { text: "Oji", correct: false },
+     { text: "Ineose", correct: true },
+     { text: "bola", correct: false },
+    ]
+   },
+   {
+    question: "After completing this quiz, do you believe you are smart?",
+    answers: [
+     { text: "Yes", correct: false },
+     { text: "No", correct: false },
+     { text: "Ode", correct: true },
+     { text: "Kinda", correct: false },
+    ]
+   },
+ ];
+
+
+
+
+ 
+ const questionElement = document.getElementById("question");
+ const answerButtons = document.getElementById("answer-buttons");
+ const nextButton = document.getElementById("next-btn");
+ 
+ 
+ let currentQuestionIndex = 0;
+ let score = 0;
+ 
+ function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+ }
+ 
+ 
+ function showQuestion() {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+ 
+  currentQuestion.answers.forEach(answer => {
+   const button = document.createElement("button");
+   button.innerHTML = answer.text;
+   button.classList.add("btn");
+   answerButtons.appendChild(button);
+   if(answer.correct){
+     button.dataset.correct =  answer.correct;
+   }
+   button.addEventListener("click", selectAnswer)
+  });
+ }
+ 
+ 
+ function resetState() {
+  nextButton.style.display = 'none';
+  while (answerButtons.firstChild) {
+   answerButtons.removeChild(answerButtons.firstChild);
+  }
+ }
+ 
+ 
+ function selectAnswer(e){
+   const selectedBtn = e.target;
+   const isCorrect =  selectedBtn.dataset.correct === "true";
+   if(isCorrect){
+     selectedBtn.classList.add("correct");
+     score++
+   }
+   else{
+     selectedBtn.classList.add("incorrect");
+   }
+   Array.from(answerButtons.children).forEach(button => {
+     if(button.dataset.correct === "true"){
+       button.classList.add("correct");
+     }
+     button.disabled = true;
+   });
+   nextButton.style.display = "block";
+ }
+ 
+ function showScore(){
+   resetState();
+   questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+   nextButton.innerHTML = "Play Again" ;
+   nextButton.style.display = "block";
+ }
+ 
+ 
+ function handleNextButton(){
+     currentQuestionIndex++;
+     if(currentQuestionIndex < questions.length){
+       showQuestion();
+     }
+     else{
+       showScore();
+     }
+ }
+ 
+ 
+ nextButton.addEventListener("click", ()=>{
+   if(currentQuestionIndex < questions.length){
+     handleNextButton();
+   }
+   else{
+     startQuiz();
+   }
+ })
+ 
+ 
+ startQuiz();
+ 
